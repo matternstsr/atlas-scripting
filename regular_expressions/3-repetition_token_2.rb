eedfd96beb62e11597e5123b6e2d6741536b1b80
matternstsr <matternstsr@gmail.com>
2025-01-07 17:35:55 -0600

    3 was still wrong

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -3,7 +3,8 @@
 # Take the argument passed to the script
 input = ARGV[0]
 # Regular expression to match 'hb' followed by one or more 't's and ending with 'n'
-if input.match?(/^hb(t{1,4})n$/)
+if input.match?(/^hb(t*)n$/)
+# if input.match?(/^hb(t{1,4})n$/)
 # Print the exact match found (i.e., the whole input string)
   puts input
 else
