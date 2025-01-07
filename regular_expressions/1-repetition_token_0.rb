929f75de809012171f963ac08842448d5ece08c8
matternstsr <matternstsr@gmail.com>
2025-01-07 16:44:36 -0600

    stupid caps button

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -4,7 +4,7 @@
 input = ARGV[0]
 #!/usr/bin/env ruby
 # Regular expression to match 'hb' followed by one or more 't's and ending with 'n'
-if input.match?(/^hb(tt+t+)n$/)
+if input.match?(/^hb(t+t+t+)n$/)
 # Print the exact match found (i.e., the whole input string)
   puts input
 else
