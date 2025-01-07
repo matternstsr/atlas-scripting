362da3bb9215576b198b9b281ee9b36efbde2070
matternstsr <matternstsr@gmail.com>
2025-01-07 17:11:36 -0600

    i swear i tried 5 already but ok

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,13 @@
 #!/usr/bin/env ruby
 
-# Take in the first argument from the command line
+# Take the argument passed to the script
 input = ARGV[0]
+#!/usr/bin/env ruby
+# Regular expression to match 'hb' followed by one or more 't's and ending with 'n'
+if input.match?(/^hb(t{1,4})n$/)
+# Print the exact match found (i.e., the whole input string)
+  puts input
+else
+# If no match, print nothing 
+  puts ""
+end
