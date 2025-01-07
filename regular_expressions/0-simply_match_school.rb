245616b3c1312e059ce7b818709350bfae091281
matternstsr <matternstsr@gmail.com>
2025-01-07 15:38:36 -0600

    added comments

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+# Take in the first argument from the command line
 input = ARGV[0]
+# Regular expression to match the word 'School'
 matches = input.scan(/School/)
+# Print all occurrences of 'School'
 puts matches.join
