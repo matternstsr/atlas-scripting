beb1482a5b7559ae79384a268e12e5df6ddd73a6
matternstsr <matternstsr@gmail.com>
2025-01-07 17:40:02 -0600

    working on 6

--- regular_expressions/6-phone_number.rb
+++ regular_expressions/6-phone_number.rb
@@ -3,7 +3,7 @@
 # Take the argument passed to the script
 input = ARGV[0]
 # Regular expression to match 'hb' followed by one or more 't's and ending with 'n'
-if input.match?(/^(1-9){10}$/)
+if input.match?(/^.{10}$/)
 # Print the exact match found (i.e., the whole input string)
   puts input
 else
