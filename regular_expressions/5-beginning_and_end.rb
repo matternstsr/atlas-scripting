3b409ba13721255d46a5f8dd8ed6eeabb19ffac9
matternstsr <matternstsr@gmail.com>
2025-01-07 17:30:42 -0600

    nesc

--- regular_expressions/5-beginning_and_end.rb
+++ regular_expressions/5-beginning_and_end.rb
@@ -2,3 +2,11 @@
 
 # Take the argument passed to the script
 input = ARGV[0]
+# Regular expression to match 'hb' followed by one or more 't's and ending with 'n'
+if input.match?(/^h(a-z)n$/)
+# Print the exact match found (i.e., the whole input string)
+  puts input
+else
+# If no match, print nothing 
+  puts ""
+end
