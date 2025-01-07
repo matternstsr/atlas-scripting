1979adb74d18ef0cb741c6445847236291e88e12
matternstsr <matternstsr@gmail.com>
2025-01-07 17:17:53 -0600

    i swear i tried 5 already but ok

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -4,7 +4,7 @@
 input = ARGV[0]
 #!/usr/bin/env ruby
 # Regular expression to match 'hb' followed by one or more 't's and ending with 'n'
-if input.match?(/^hb(t*)o{0}$/)
+if input.match?(/^hb(t*o{0})$/)
 # Print the exact match found (i.e., the whole input string)
   puts input
 else
