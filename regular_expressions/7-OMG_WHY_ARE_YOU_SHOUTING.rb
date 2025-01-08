4693f96a8734184051167c17ff5b3a73c8200d60
matternstsr <matternstsr@gmail.com>
2025-01-07 18:18:57 -0600

    scanning

--- regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -3,11 +3,12 @@
 # Take the argument passed to the script
 input = ARGV[0]
 # Regular expression to match 'hb' followed by one or more 't's and ending with 'n'
-if input.match?(/^(A-Z)$/)
+result = input.scan(/[A-Z]/).join
 # could have used \d*
 # Print the exact match found (i.e., the whole input string)
   puts input
 else
 # If no match, print nothing
-  puts "HBTN"
+  puts ""
+  #  puts "HBTN" works too lol
 end
