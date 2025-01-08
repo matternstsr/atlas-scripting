3c8dbc4a2105dbe8c3d2562bb116b4d5d6135379
matternstsr <matternstsr@gmail.com>
2025-01-07 18:30:55 -0600

    hmmm

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,4 +1,18 @@
 #!/usr/bin/env ruby
 
-# Take in the first argument from the command line
+# Read the input from ARGV (argument passed to the script)
 input = ARGV[0]
+
+# Regular expression to match and extract the sender, receiver, and flags
+match = input.match(/^([^\s,]+),([^\s,]+)(?:,(.+))?$/)
+# Check if a match was found
+if match
+  sender = match[1]
+  receiver = match[2]
+  flags = match[3] ? match[3] : ""
+# Output the required format
+  puts "#{sender},#{receiver},#{flags}"
+else
+# If the input does not match print nothing
+  puts ""
+end
